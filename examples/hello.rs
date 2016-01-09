7cfe183b54c24989046f2127622713be6ef599cb
Daniel Keep <daniel.keep@gmail.com>
2016-01-09 16:20:55 +1100

    Moved src/bin/hello.rs to examples/

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,6 @@
-// TODO: Make an example, instead.
-
 fn main() {
     { extern crate ansi_interpreter as ai; ai::intercept_stdio(); }
-    print!("\x1b]2;Hello, World! ☺\x07");
+    print!("\x1b]2;Hello, World! ☺ ℌ𝔬𝔬𝔯𝔞𝔶 𝔘𝔫𝔦𝔠𝔬𝔡𝔢!\x07");
     println!("\x1b[0;43;91mH\x1b[22mello\x1b[39m, \x1b[92mW\x1b[22morld\x1b[94m!\x1b[m");
     std::thread::sleep(std::time::Duration::from_millis(1000));
 }

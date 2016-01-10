95af2a72449e51a0639a400bf7c5938e5964add9
Daniel Keep <daniel.keep@gmail.com>
2016-01-10 15:40:12 +1100

    Rejiggered public exports.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,20 @@ extern crate conv;
 extern crate num;
 extern crate smallvec;
 
-pub use ansi::{AnsiIntercept, EraseDisplay, EraseLine, AnsiInterpret};
-
-// TODO: reconsider exporting this.
-pub use win32::intercept_stdio;
+pub use export::*;
 
 #[macro_use] mod macros;
 
 mod ansi;
 mod util;
 mod win32;
+
+mod export {
+    pub use ansi::{AnsiIntercept, EraseDisplay, EraseLine, AnsiInterpret};
+
+    #[cfg(windows)]
+    pub use win32::intercept_stdio;
+
+    #[cfg(not(windows))]
+    pub fn intercept_stdio() {}
+}
